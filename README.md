78baa393f87eef4d852c47c98d007fa2f2678e51
Viktar Palstsiuk <viktar.palstsiuk@promwad.com>
2018-06-08 11:48:31 +0300

    Match with grandparents segments

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # DNA relatives grouping
 The group.py script is used to group 23andMe DNA relatives with ovelapping segments.
 
-The plot23.py script is used to plot 23 chromosomes with colorized group segments.
-
 ## Requirements
 * Python2.7
 
@@ -11,13 +9,11 @@ Go to https://you.23andme.com/tools/relatives/ and Download aggregate data.
 
 ```
 python group.py <INPUT_CSV>
-python plot23.py grp_<INPUT_CSV>
 ```
 
-Open created PNG file.
+Open created CSV file.
 
 ## Usage Example
 ```
 python group.py john_smith_relatives_download.csv
-python plot23.py grp_john_smith_relatives_download.csv
 ```
--- group.py
+++ group.py
@@ -10,24 +10,23 @@ f = open(filename, 'rb')
 print "Input:", filename
 reader = csv.reader(f)
 your_list = list(reader)
-elements = len(your_list[0])
-print 'Line elements:', elements
-your_list[0].append( "Group" );
 print 'Group name element:', len(your_list[0])
 print 'Lines:', len(your_list)
 
-groups = {}
+f2 = open('vfp.csv', 'rb')
+reader2 = csv.reader(f2)
+vfp_list = list(reader2)
+
+f3 = open('nkk.csv', 'rb')
+reader3 = csv.reader(f3)
+nkk_list = list(reader3)
 
 ofile  = open("grp_" + filename, "wb")
 writer = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+your_list[0].pop(8)
+your_list[0].insert(3,"Pat")
+your_list[0].insert(4,"Mat")
 writer.writerow(your_list[0])
-
-minimal_snp=700
-minimal_length=7
-minimal_overlap=0.2
-print 'minimal_snp:',minimal_snp
-print 'minimal_length (cM):',minimal_length
-print 'minimal_overlap:',minimal_overlap
 
 for row1 in your_list:
 	if not row1[3].isdigit(): continue
@@ -36,52 +35,35 @@ for row1 in your_list:
 	if chro1 == '': continue
 	beg1=int(row1[3])
 	end1=int(row1[4])
-	cM1=float(row1[5])
-	snp1=int(row1[6])
-	#seg1=int(row1[15])
-	if snp1 < minimal_snp: continue
-	if cM1 < minimal_length: continue
+	res = "Vera"
+	res2 = "Petr"
 
-	relatives = set()
+	for row2 in vfp_list:
+		if not row2[0].isdigit(): continue
+		chro2=row2[0]
+		beg2=int(row2[1])
+		end2=int(row2[2])
+		#print chro1, beg1, end1, chro2, beg2, end2
 
-	for row2 in your_list:
-		if not row2[3].isdigit(): continue
-		name2=row2[0]
-		chro2=row2[2]
-		if chro2 == '': continue
-		beg2=int(row2[3])
-		end2=int(row2[4])
-		cM2=float(row2[5])
-		snp2=int(row2[6])
-		#seg2=int(row2[15])
-		if snp2 < minimal_snp: continue
-		if cM2 < minimal_length: continue
+		if (chro1 == chro2 and getOverlap([beg1, end1], [beg2, end2]) >= 1.0):
+			res = "Wkrt"
 
-		if (chro1 == chro2 and getOverlap([beg1, end1], [beg2, end2]) >= minimal_overlap):
-			relatives.add(name2)
-	groups.update({name1: relatives})
+	for row2 in nkk_list:
+		if not row2[0].isdigit(): continue
+		chro2=row2[0]
+		beg2=int(row2[1])
+		end2=int(row2[2])
+		#print chro1, beg1, end1, chro2, beg2, end2
 
-for k in groups.keys():
-	if k in groups:
-		for j in groups.keys():
-			if k == j: continue
-			c = groups[k].intersection(groups[j])
-			if bool(c):
-				groups[k].update(groups[j])
-				del groups[j]
-
-for k in groups.keys():
-	if len(groups[k]) > 1:
-		print k, '\t', groups[k]
-
-for row1 in your_list:
-	for k in groups.keys():
-		name1=row1[0]
-		if name1 in groups[k]:
-			row1.append(k)
-			writer.writerow(row1)
+		if (chro1 == chro2 and getOverlap([beg1, end1], [beg2, end2]) >= 1.0):
+			res2 = "Nina"
+	
+	row1.pop(8)
+	row1.insert(3,res)
+	row1.insert(4,res2)
+	writer.writerow(row1)
 
 f.close()
-
-print 'Groups:', len(groups)
+f2.close()
+f3.close()
 
